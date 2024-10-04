a7b619bfde3b5d685749b6e6c366d611785aa613
Laurent Ouma <lawiomosh3@gmail.com>
2024-10-04 11:14:08 +0300

    WIP

--- packages/esm-patient-clinical-view-app/src/hooks/useRelationship.ts
+++ packages/esm-patient-clinical-view-app/src/hooks/useRelationship.ts
@@ -3,7 +3,7 @@ import useSWR from 'swr';
 import { Relationship } from '../types';
 
 const useRelationship = (relationshipUuid: string) => {
-  const customRepresentation = `custom:(display,uuid,startDate,endDate,relationshipType:(uuid,display,aIsToB,bIsToA))`;
+  const customRepresentation = `custom:(display,uuid,startDate,endDate,relationshipType:(uuid,display,aIsToB,bIsToA),personA:(uuid,age,dead,display,causeOfDeath,gender,attributes:(uuid,display,value,attributeType:(uuid,display))),personB:(uuid,age,dead,display,causeOfDeath,gender,attributes:(uuid,display,value,attributeType:(uuid,display))))`;
   const url = `${restBaseUrl}/relationship/${relationshipUuid}?v=${customRepresentation}`;
   const { isLoading, data, error } = useSWR<FetchResponse<Relationship>>(url, openmrsFetch);
   return {
--- packages/esm-patient-clinical-view-app/src/relationships/forms/relationships-update-form.workspace.tsx
+++ packages/esm-patient-clinical-view-app/src/relationships/forms/relationships-update-form.workspace.tsx
@@ -11,7 +11,7 @@ import {
   Tile,
 } from '@carbon/react';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { DefaultWorkspaceProps, showSnackbar } from '@openmrs/esm-framework';
+import { DefaultWorkspaceProps, showSnackbar, usePatient } from '@openmrs/esm-framework';
 import React, { useEffect } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
@@ -93,6 +93,7 @@ const RelationshipUpdateForm: React.FC<RelationshipUpdateFormProps> = ({ closeWo
   return (
     <Form onSubmit={form.handleSubmit(onSubmit)}>
       <Stack gap={4} className={styles.grid}>
+        <Column></Column>
         <Column>
           <Controller
             control={form.control}
