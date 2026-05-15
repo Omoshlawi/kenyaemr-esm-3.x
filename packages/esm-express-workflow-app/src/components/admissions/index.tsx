fce35d38045865b7da6754d729bcc34aebe3b6e5
Laurent Ouma <98098891+Omoshlawi@users.noreply.github.com>
2026-05-15 20:44:24 +0300

    fix: Fix inpatient  pannel layout and moved summary related code to seperate component, (#128)

--- packages/esm-express-workflow-app/src/components/admissions/index.tsx
+++ packages/esm-express-workflow-app/src/components/admissions/index.tsx
@@ -8,6 +8,7 @@ const options = {
 };
 
 export const admissionsDashboard = getAsyncLifecycle(() => import('./admissions-dashboard.component'), options);
+export const admissionsMedicationSummary = getAsyncLifecycle(() => import('./admission-medication-summary'), options);
 // t('inPatient', 'In-Patient')
 export const admissionsDashboardLink = getAsyncLifecycle(
   () =>
